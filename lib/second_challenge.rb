b22d3d264c217497ef46e8c4c06d23860c52cfa1
Sean Conrad <seanconrad123@gmail.com>
2018-06-22 23:15:17 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
+      :patriarch => {  
         :name => "Lord Montague",
         :age => "53"
-      }
-      },
+    },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+    },
+      :hero => {
+        :name => "Romeo",
+        :age => "15"
+    },
     :capulet => {
       #your key/value pairs here!
       }}
