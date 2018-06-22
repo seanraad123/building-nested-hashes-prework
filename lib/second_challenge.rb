8b68a8703b25ca484f1d2209d56cc2d7d3a89594
Sean Conrad <seanconrad123@gmail.com>
2018-06-22 23:22:29 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -31,7 +31,10 @@ def second_challenge
         name: "Lord Capulet", 
         age: "50"
       },
-      :matriarch => {name: "Lady Capulet", age: "51"},
+      :matriarch => {
+        name: "Lady Capulet", 
+        age: "51"
+      },
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"},
